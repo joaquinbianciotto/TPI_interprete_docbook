b1ac22a0e14e815f178fa7dc6994bb8f3e53412a
Joaquin Bianciotto <joaquinbianciotto123@hotmail.com>
2023-06-03 15:59:37 -0300

    cambios esteticos, agregada funcion borrarpantalla

--- src/main.py
+++ src/main.py
@@ -1,26 +1,38 @@
 from lexer import lexer
 import os
 import sys
+def borrarPantalla(): #Borra lo ya escrito en pantalla
+      if os.name == "posix":
+            os.system ("clear")
+      elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+            os.system ("cls")
 print ("Hola este es el analizador Lexico")
-print ("Ingrese 1 si quiere ingresar datos a mano y 2 si quiere desde un archivo de prueba \n")
+print ("1 para ingresar datos a mano\n2 si quiere cargar datos desde un archivo\n")
 op = input()
 errores = []
 if op == "1":                                               #ingreso manual
-      print("ingrese lo que quiere analizar")
-      cadena = input()    
-      lexer.input(cadena)
-      while True:
-            tok = lexer.token()
-            
-            if not tok : break
-            print (tok)
+      borrarPantalla()
+      while True: #ciclo para ingresar datos hasta que eleccion sea 0
+            print("ingrese lo que quiere analizar")
+            cadena = input()    
+            lexer.input(cadena)
+            while True:
+                  tok = lexer.token()
+
+                  if not tok : break
+                  if tok.type == "ERROR_1" or tok.type == "ERROR_2" or tok.type == "ERROR_3":
+                        print(f"error lexico en linea {tok.lineno} ({tok.value})")
+                  else:
+                        print(tok)
+            print("desea continuar?\n1 para continuar\n0 para terminar")
+            eleccion = input()
+            if eleccion == "0": break
+            borrarPantalla()
 
 elif op == "2":                                       #ingreso por archivo
-      print("todavia no esta listo")
       n = 0
-      
-      ejemplo_dir = 'prueba/'                   #elegir el archivo
-      with os.scandir(ejemplo_dir) as ficheros:
+      dir = 'prueba/'                   #elegir el archivo
+      with os.scandir(dir) as ficheros:
             print(type(ficheros))
             ficheros = [fichero.name for fichero in ficheros if fichero.is_file()]    #ficheros es una lista con los archivos de la carpeta prueba
       for j in ficheros:
@@ -30,12 +42,13 @@ elif op == "2":                                       #ingreso por archivo
             
             print(f"{n+1}: {ficheros[n]}")
             n +=1
-      print("elegi el archivo")
+      print("elegi el archivo para leer")
       op2 = input()
+      borrarPantalla()
       if int(op2) <= n:
             ruta = ficheros[int(op2)-1]
             with open(f"prueba/{ruta}","r",encoding="utf-8") as maestro: #esto ya funciona para cualquier fichero en prueba/
-                  print(f"hola abri el archivo: {ruta}")
+                  print(f"abierto archivo: {ruta}")
                   lexer.input(maestro.read())               
                   while True:
                         tok = lexer.token()
