d7aadb275667c94a59bcc71d845f9f72632be859
Joaquin Bianciotto <joaquinbianciotto123@hotmail.com>
2023-06-27 19:37:51 -0300

    cambios en archivos, agregado main principal

---
+++ src/main1.py
@@ -0,0 +1,61 @@
+from lexer import lexer
+from parser1 import parser, correcto
+import os
+import sys
+from lexer import arch
+from pruebaLexer import borrarPantalla
+
+print ("Hola este es el analizador Sintactico")
+print ("1 para ingresar datos a mano\n2 si quiere cargar datos desde un archivo\n")
+op = input()
+errores = []
+lexico = 0
+if op == "1":                                               #ingreso manual
+    borrarPantalla()
+    while True: #ciclo para ingresar datos hasta que eleccion sea 0
+        print("ingrese lo que quiere analizar")
+        cadena = input()    
+        parser.parse(cadena)
+           
+        print("desea continuar?\n1 para continuar\n0 para terminar")
+        eleccion = input()
+        if eleccion == "0": break
+        borrarPantalla()
+elif op == "2":                                       #ingreso por archivo
+    n = 0
+    dir = 'prueba/'                   #elegir el archivo
+    with os.scandir(dir) as ficheros:
+        print(type(ficheros))
+        ficheros = [fichero.name for fichero in ficheros if fichero.is_file()]    #ficheros es una lista con los archivos de la carpeta prueba
+    for j in ficheros:
+        if ".txt" in j:
+            ficheros.remove(f"{j}")
+    for i in ficheros:
+            
+        print(f"{n+1}: {ficheros[n]}")
+        n +=1
+    print("elegi el archivo para leer")
+    op2 = input()
+    borrarPantalla()
+    if int(op2) <= n:
+        ruta = ficheros[int(op2)-1]
+        with open(f"prueba/{ruta}","r",encoding="utf-8") as maestro: #esto ya funciona para cualquier fichero en prueba/
+            parser.parse(maestro.read())
+
+            cambio = ruta.replace(".xml","")
+            with os.scandir('src/html_generados/') as htmls:
+                for k in htmls:
+                        if k.name == f"{cambio}.html":
+                            os.remove(f"src/html_generados/{cambio}.html")
+            os.rename("src/html_generados/archivo.html",f"src/html_generados/{cambio}.html" )
+            os.system("pause")
+    else:
+        print("numero invalido")
+        os.system("pause")
+else:
+      print("volve a empezar")
+      os.system("pause")
+
+
+                  
+                
